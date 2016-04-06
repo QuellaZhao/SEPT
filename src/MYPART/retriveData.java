df0f2c12c2ce0dd444332df6a92354e476172d0e
QuellaZhao <s3569074@student.rmit.edu.au>
2016-04-07 00:16:23 +1000

    Update retriveData.java

--- src/MYPART/retriveData.java
+++ src/MYPART/retriveData.java
@@ -1,60 +1,41 @@
 package MYPART;
 
-import java.io.*;
-import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-//import org.apache.commons.io.FileUtils; 
-import org.json.*;
-import org.json.simple.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 
 public class retriveData {
-	/*public static String downloadFromUrl(String url,String dir) {  
-		  
-        try {  
-            URL httpurl = new URL(url);  
-            String fileName = "temp.json";  
-            System.out.println(fileName);  
-            File f = new File(dir + fileName);  
-            FileUtils.copyURLToFile(httpurl, f);  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-            return "Fault!";  
-        }   
-        return "Successful!";  
-    }
-	*/
-public void justtry(){
-	
-	/*StringBuffer stringBuffer = new StringBuffer();
-	String line = null ;
+	public static void main(String[] args){
 	try {
-	BufferedReader br = new BufferedReader(new FileReader(new File("C:/Users/Administrator/Desktop/tep.json")));
-	while( (line = br.readLine())!= null ){
-	stringBuffer.append(line);
-	} 
-	} catch (FileNotFoundException e) {
-	       e.printStackTrace();
-	} catch (IOException e) {
-	e.printStackTrace();
-	}*/
-	
-	JSONParser parser = new JSONParser();
-	
-	
-	JSONObject jsonObject = new JSONObject(stringBuffer.toString());
-	
-	String air_temp = null;
-	StringBuffer jsonFileInfo = new StringBuffer();
-		air_temp = jsonObject.getJSONObject("observations").toString();
+		JsonParser parser = new JsonParser();
+		JsonObject object = (JsonObject) parser.parse(new FileReader("stations.json"));
+		JsonArray array = object.get("Array").getAsJsonArray();
+		for(int i = 0; i<array.size(); i++){
+			JsonObject obj = array.get(i).getAsJsonObject();
+			System.out.println("state: " + obj.get("state").getAsString());
+			
+			JsonArray ar = obj.get("stations").getAsJsonArray();
+			for(int j = 0; j<ar.size(); j++){
+				JsonObject subobj = ar.get(j).getAsJsonObject();
+				System.out.println("city: " + subobj.get("city").getAsString());
+				System.out.println("url: " + subobj.get("url").getAsString());
+			}
+			System.out.println("================================");
+		}
 		
-		String des = air_temp.substring(air_temp.indexOf("\"air_temp\":"), air_temp.indexOf(",\"local_date_time_full\":"));
-
-		      System.out.print("air tempreture:" + des +"\n");
+		
+		
+	} catch (JsonIOException e) {
+		e.printStackTrace();
+	} catch (JsonSyntaxException e) {
+		e.printStackTrace();
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
 	}
-public static void main(String[] args){
-	retriveData rd = new retriveData();
-	rd.justtry();
-	
-}
+	}
 }
