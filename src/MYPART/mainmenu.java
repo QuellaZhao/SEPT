7386c14f5faa6cbfe707aa3bf020b702ffe39135
rmit-s3569074-Dan-Zhao <s3569074@student.rmit.edu.au>
2016-04-15 14:18:48 +1000

    Update mainmenu.java

--- src/MYPART/mainmenu.java
+++ src/MYPART/mainmenu.java
@@ -1,9 +1,15 @@
 package MYPART;
 
+import java.io.FileNotFoundException;
+
 import javax.swing.*;
 
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+
 public class mainmenu extends JFrame{
-	public static void main(String[] args){
+	public static void main(String[] args) throws JsonIOException, JsonSyntaxException, FileNotFoundException{
 		mainframe mf = new mainframe();
+		mf.mframe();
 	}
 	}
