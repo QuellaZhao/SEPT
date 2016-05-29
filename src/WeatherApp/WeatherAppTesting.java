b95dd87f41bc4c5b01a818826133e35c06351c2d
Akshay Waghule <s3470510@student.rmit.edu.au>
2016-05-30 04:42:08 +1000

    Added some more test cases

--- src/WeatherApp/WeatherAppTesting.java
+++ src/WeatherApp/WeatherAppTesting.java
@@ -1,25 +1,14 @@
 package WeatherApp;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
 import java.io.IOException;
 import java.net.MalformedURLException;
-
-import org.junit.Test;
 
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 
 public class WeatherAppTesting {
-	
-	//retrive data
-	//add button
-	//station button
-	//remove button
-	//ok button
-	//graph button
-	//refresh button?!!
-	//
 	
 	@Test
 	public void addFavTest() throws JsonIOException, JsonSyntaxException, MalformedURLException, IOException {
@@ -37,6 +26,27 @@ public class WeatherAppTesting {
 		assertEquals("Melbourne Airport",favs.getFavs().get(favs.favs.size() - 1));
 	}	
 	
+	//tests don't working
+	@Test
+	public void URLHashMapTest1() throws JsonIOException, JsonSyntaxException, MalformedURLException, IOException{
+		retriveDatafromBOM rb = new retriveDatafromBOM();
+		rb.getWeatherData("York");
+		assertEquals("http://www.bom.gov.au/fwo/IDW60801/IDW60801.94623.json", rb.getURLForStation().get("York").toString());
+	}
 	
+	@Test
+	public void URLHashMapTest2() throws JsonIOException, JsonSyntaxException, MalformedURLException, IOException{
+		retriveDatafromBOM rb = new retriveDatafromBOM();
+		rb.getWeatherData("Melbourne Airport");
+		assertEquals("http://www.bom.gov.au/fwo/IDV60801/IDV60801.94866.json", rb.getURLForStation().get("Melbourne Airport"));
+
+	}
 	
+	@Test
+	public void URLHashMapTest3() throws JsonIOException, JsonSyntaxException, MalformedURLException, IOException{
+		retriveDatafromBOM rb = new retriveDatafromBOM();
+		rb.getWeatherData("Batemans Bay");
+		assertEquals("http://www.bom.gov.au/fwo/IDN60903/IDN60903.94941.json", rb.getURLForStation().get("Batemans Bay"));
+
+	}
 }
